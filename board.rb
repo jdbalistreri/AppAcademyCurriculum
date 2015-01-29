48bf99f052e20dfe108daf6415ab367ac24bc2c7
Joe Balistreri <jdbalistreri@gmail.com>
2015-01-29 10:18:52 -0500

    add scaffolding for piece methods, add move_diffs method to piece class.

--- board.rb
+++ board.rb
@@ -2,9 +2,20 @@ require_relative "checkers.rb"
 
 class Board
 
-  def initialize
-
+  def initialize #this could take an empty argument
+    fill_board #make this an array of pieces, need a fill board method
   end
 
+  def fill_board #this could take a true/false value
+    @board = []
+  end
+
+  def add_piece(piece)
+    #this takes a piece object and then will place it using the pieces coordinates
+  end
+
+  def remove_piece(position)
+    #this should remove a piece, for instance, after a jump
+  end
 
 end
--- pieces.rb
+++ pieces.rb
@@ -1,9 +1,61 @@
 require_relative "checkers.rb"
 
 class Piece
+  attr_reader :color, :board, :position, :king
 
-  def initialize
+  RED_DIRS   = [[-1,1], [-1,-1]]
+  BLACK_DIRS = [[ 1,1], [ 1,-1]]
 
+  def initialize(color, board, position)
+    @color = color
+    @board = board
+    @position = position
+    @king = false
+
+    #make it put itself on the board @board.add_piece(self)
+  end
+
+  def move(direction)
+    if perform_slide(direction)
+      "Slide performed!"
+    elsif perform_jump(direction)
+      "Jump performed!"
+    else
+      "Not a valid direction"
+    end
+  end
+
+  def perform_slide(direction)
+    #check move dirs
+    #return true or false
+  end
+
+  def perform_jump
+
+    #return true or false
+  end
+
+  def move_diffs
+    possible_directions = []
+    if king
+      possible_directions.concat(RED_DIRS).concat(BLACK_DIRS)
+    elsif color == :red
+      possible_directions.concat(RED_DIRS)
+    else
+      possible_directions.concat(BLACK_DIRS)
+    end
+  end
+
+  def on_the_board?(pos)
+    pos.all? { |coord| coord.between?(0,7) }
+  end
+
+  def promote
+    @king = true
+  end
+
+  def maybe_promote
+    #should ask if it's in the back row - if so, it should promote;
   end
 
 
