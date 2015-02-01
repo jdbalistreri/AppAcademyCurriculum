4a04f16c90f34b504395dff3db0d5d9460d76dd7
Joe Balistreri <jdbalistreri@gmail.com>
2015-02-01 17:47:42 -0500

    add additional logic to hand class

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -4,8 +4,18 @@ require_relative 'deck'
 class Hand
   attr_accessor :cards
 
-  def initialize(deck, cards = nil)
-    @deck = deck
+  HAND_RANKS = {straight_flush: 1,
+                four_of_a_kind: 2,
+                full_house: 3,
+                flush: 4,
+                straight: 5,
+                three_of_a_kind: 6,
+                two_pair: 7,
+                one_pair: 8,
+                high_card: 9}
+
+  def initialize(deck = nil, cards = nil)
+    @deck = deck || Deck.new
     @cards = cards || []
   end
 
@@ -28,12 +38,54 @@ class Hand
     @deck.receive_cards(discarded)
   end
 
+  def beats?(other_hand)
+    HAND_RANKS[hand_type] < HAND_RANKS[other_hand.hand_type]
+  end
+
+  def hand_type
+    if straight_flush?
+      :straight_flush
+    elsif four_of_a_kind?
+      :four_of_a_kind
+    elsif full_house?
+      :full_house
+    elsif flush?
+      :flush
+    elsif straight?
+      :straight
+    elsif three_of_a_kind?
+      :three_of_a_kind
+    elsif two_pair?
+      :two_pair
+    elsif one_pair?
+      :one_pair
+    else
+      :high_card
+    end
+  end
+
   def straight_flush?
     flush? && straight?
   end
 
-  def full_house?
+  def four_of_a_kind?
+    num_of_a_kind.include?(4)
+  end
 
+  def full_house?
+    num_of_a_kind.include?(3) && num_of_a_kind.include?(2)
+  end
+
+  def three_of_a_kind?
+    num_of_a_kind.include?(3) && !full_house?
+  end
+
+  def two_pair?
+    num_of_a_kind.count(2) == 2 && !full_house?
+  end
+
+  def one_pair?
+    num_of_a_kind.include?(2) && !two_pair? && !full_house?
   end
 
   def flush?
@@ -52,8 +104,6 @@ class Hand
       end
       true
     end
-
-
   end
 
   def high_card_value
@@ -67,7 +117,14 @@ class Hand
   end
 
   def num_of_a_kind
-    
+    card_values.values
+  end
+
+  def card_values
+    card_values = Hash.new(0)
+
+    @cards.map(&:poker_value).each { |val| card_values[val] += 1 }
+    card_values
   end
 
 end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -46,70 +46,230 @@ describe Hand do
     end
   end
 
-  describe "#flush?" do
-    it "returns true when a hand is a flush" do
-      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
-                    Card.new(:spades, :four), Card.new(:spades, :eight),
-                    Card.new(:spades, :six)])
-      expect(hand.flush?).to be(true)
+  context "Types of Hands" do
+    describe "#flush?" do
+      it "returns true when a hand is a flush" do
+        hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                      Card.new(:spades, :four), Card.new(:spades, :eight),
+                      Card.new(:spades, :six)])
+        expect(hand.flush?).to be(true)
+      end
+
+      it "returns false when a hand is not a flush" do
+        hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                      Card.new(:hearts, :four), Card.new(:spades, :five),
+                      Card.new(:spades, :six)])
+        expect(hand.flush?).to be(false)
+      end
+    end
+    describe "#straight?" do
+      it "returns true when a hand is a straight" do
+        hand = Hand.new(deck, [Card.new(:spades, :ten), Card.new(:spades, :jack),
+                      Card.new(:hearts, :ace), Card.new(:spades, :queen),
+                      Card.new(:spades, :king)])
+        expect(hand.straight?).to be(true)
+      end
+
+      it "returns false when a hand is not a straight" do
+        hand = Hand.new(deck, [Card.new(:spades, :ace), Card.new(:spades, :three),
+                      Card.new(:hearts, :four), Card.new(:spades, :deuce),
+                      Card.new(:hearts, :deuce)])
+        expect(hand.straight?).to be(false)
+      end
     end
 
-    it "returns false when a hand is not a flush" do
-      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
-                    Card.new(:hearts, :four), Card.new(:spades, :five),
-                    Card.new(:spades, :six)])
-      expect(hand.flush?).to be(false)
+    describe "#straight_flush?" do
+      it "returns true when a hand is a straight flush" do
+        hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                      Card.new(:spades, :four), Card.new(:spades, :five),
+                      Card.new(:spades, :six)])
+        expect(hand.straight_flush?).to be(true)
+      end
+
+      it "returns false when a hand is just a flush" do
+        hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                      Card.new(:spades, :four), Card.new(:spades, :eight),
+                      Card.new(:spades, :six)])
+        expect(hand.straight_flush?).to be(false)
+      end
+
+      it "returns false when a hand is just a straight" do
+        hand = Hand.new(deck, [Card.new(:spades, :ten), Card.new(:spades, :jack),
+                      Card.new(:hearts, :ace), Card.new(:spades, :queen),
+                      Card.new(:spades, :king)])
+        expect(hand.straight_flush?).to be(false)
+      end
+    end
+
+
+    describe "#four_of_a_kind?" do
+      it "returns true when a hand has four of a kind" do
+        hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                      Card.new(:hearts, :three), Card.new(:clubs, :three),
+                      Card.new(:diamonds, :three)])
+        expect(hand.four_of_a_kind?).to be(true)
+      end
+
+      it "returns false when a hand does not have four of a kind" do
+        hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                      Card.new(:hearts, :three), Card.new(:clubs, :three),
+                      Card.new(:diamonds, :four)])
+        expect(hand.four_of_a_kind?).to be(false)
+      end
+    end
+
+    describe "#full_house?" do
+      it "returns true when a hand has a full house" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :three), Card.new(:clubs, :deuce),
+                      Card.new(:diamonds, :deuce)])
+        expect(hand.full_house?).to be(true)
+      end
+
+      it "returns false when a hand does not have a full house" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :five), Card.new(:clubs, :deuce),
+                      Card.new(:diamonds, :deuce)])
+        expect(hand.full_house?).to be(false)
+      end
+    end
+
+    describe "#three_of_a_kind?" do
+      it "returns true when a hand has three of a kind" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :three), Card.new(:clubs, :deuce),
+                      Card.new(:diamonds, :five)])
+        expect(hand.three_of_a_kind?).to be(true)
+      end
+
+      it "returns false when a hand is a full house" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :three), Card.new(:clubs, :deuce),
+                      Card.new(:diamonds, :deuce)])
+        expect(hand.three_of_a_kind?).to be(false)
+      end
+
+      it "returns false when a hand has fewer than three of a kind" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :five), Card.new(:clubs, :deuce),
+                      Card.new(:diamonds, :deuce)])
+        expect(hand.three_of_a_kind?).to be(false)
+      end
+    end
+
+    describe "#two_pair?" do
+      it "returns true when a hand has two pairs" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :deuce), Card.new(:clubs, :deuce),
+                      Card.new(:diamonds, :five)])
+        expect(hand.two_pair?).to be(true)
+      end
+
+      it "returns false when a hand is a full house" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :three), Card.new(:clubs, :deuce),
+                      Card.new(:diamonds, :deuce)])
+        expect(hand.two_pair?).to be(false)
+      end
+
+      it "returns false when one pair" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :five), Card.new(:clubs, :six),
+                      Card.new(:diamonds, :seven)])
+        expect(hand.two_pair?).to be(false)
+      end
+    end
+
+
+    describe "#one_pair?" do
+      it "returns true when a hand has one pair" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :deuce), Card.new(:clubs, :six),
+                      Card.new(:diamonds, :five)])
+        expect(hand.one_pair?).to be(true)
+      end
+
+      it "returns false when a hand is a full house" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :three), Card.new(:clubs, :deuce),
+                      Card.new(:diamonds, :deuce)])
+        expect(hand.one_pair?).to be(false)
+      end
+
+      it "returns false when a hand has two pair" do
+        hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                      Card.new(:hearts, :five), Card.new(:clubs, :five),
+                      Card.new(:diamonds, :seven)])
+        expect(hand.one_pair?).to be(false)
+      end
     end
   end
-  describe "#straight?" do
-    it "returns true when a hand is a straight" do
-      hand = Hand.new(deck, [Card.new(:spades, :ten), Card.new(:spades, :jack),
-                    Card.new(:hearts, :ace), Card.new(:spades, :queen),
-                    Card.new(:spades, :king)])
-      expect(hand.straight?).to be(true)
-    end
 
-    it "returns false when a hand is not a straight" do
-      hand = Hand.new(deck, [Card.new(:spades, :ace), Card.new(:spades, :three),
-                    Card.new(:hearts, :four), Card.new(:spades, :deuce),
-                    Card.new(:hearts, :deuce)])
-      expect(hand.straight?).to be(false)
-    end
-  end
-
-  describe "#straight_flush?" do
-    it "returns true when a hand is a straight flush" do
+  describe "#hand_type" do
+    it "correctly identifies a straight flush" do
       hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
                     Card.new(:spades, :four), Card.new(:spades, :five),
                     Card.new(:spades, :six)])
-      expect(hand.straight_flush?).to be(true)
+      expect(hand.hand_type).to be(:straight_flush)
     end
 
-    it "returns false when a hand is just a flush" do
-      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
-                    Card.new(:spades, :four), Card.new(:spades, :eight),
-                    Card.new(:spades, :six)])
-      expect(hand.straight_flush?).to be(false)
-    end
-
-    it "returns false when a hand is just a straight" do
+    it "correctly identifies a straight" do
       hand = Hand.new(deck, [Card.new(:spades, :ten), Card.new(:spades, :jack),
                     Card.new(:hearts, :ace), Card.new(:spades, :queen),
                     Card.new(:spades, :king)])
-      expect(hand.straight_flush?).to be(false)
+      expect(hand.hand_type).to be(:straight)
+    end
+
+    it "correctly identifies a flush" do
+      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                    Card.new(:spades, :four), Card.new(:spades, :five),
+                    Card.new(:spades, :nine)])
+      expect(hand.hand_type).to be(:flush)
+    end
+
+    it "correctly identifies a four of a kind" do
+      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                    Card.new(:hearts, :three), Card.new(:clubs, :three),
+                    Card.new(:diamonds, :three)])
+      expect(hand.hand_type).to be(:four_of_a_kind)
+    end
+
+    it "correctly identifies a full house" do
+      hand = Hand.new(deck, [Card.new(:diamonds, :three), Card.new(:spades, :three),
+                    Card.new(:hearts, :three), Card.new(:clubs, :deuce),
+                    Card.new(:diamonds, :deuce)])
+      expect(hand.hand_type).to be(:full_house)
+    end
+
+    it "correctly identifies a three of a kind" do
+      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                    Card.new(:hearts, :three), Card.new(:clubs, :three),
+                    Card.new(:diamonds, :four)])
+      expect(hand.hand_type).to be(:three_of_a_kind)
+    end
+
+    it "correctly identifies a two pair" do
+      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :four),
+                    Card.new(:hearts, :three), Card.new(:clubs, :three),
+                    Card.new(:diamonds, :four)])
+      expect(hand.hand_type).to be(:two_pair)
+    end
+
+    it "correctly identifies one pair" do
+      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :four),
+                    Card.new(:hearts, :seven), Card.new(:clubs, :three),
+                    Card.new(:diamonds, :four)])
+      expect(hand.hand_type).to be(:one_pair)
+    end
+
+    it "correctly identifies high card" do
+      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :four),
+                    Card.new(:hearts, :seven), Card.new(:clubs, :three),
+                    Card.new(:diamonds, :nine)])
+      expect(hand.hand_type).to be(:high_card)
     end
   end
 
 
-  describe "#num_of_a_kind" do
-    # it "returns the correct number of a kind" do
-    #   hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
-    #                 Card.new(:spades, :four), Card.new(:spades, :five),
-    #                 Card.new(:spades, :six)])
-    #   expect(hand.straight_flush?).to be(true)
-    # end
-
-
-  end
 
 end
