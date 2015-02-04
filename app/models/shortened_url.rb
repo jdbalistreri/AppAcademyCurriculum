6288104c9c46c711d9bb19154123d02e438fbde1
Joe Balistreri <jdbalistreri@gmail.com>
2015-02-04 12:09:53 -0500

    Add random code generation for shortening urls.

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -5,4 +5,18 @@ class ShortenedUrl < ActiveRecord::Base
 
   validates :short_url, :uniqueness => true
 
+  def self.random_code
+    loop do
+      code = SecureRandom::urlsafe_base64
+      return code unless self.exists?(:short_url => code)
+    end
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    raise 'Not a user object.' unless user.is_a?(User)
+    self.create!(:long_url => long_url,
+                  :short_url => random_code,
+                  :submitter_id => user.id)
+  end
+
 end
