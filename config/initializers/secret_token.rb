8f6769b5a6c19b6b1110ddce5d0fe677411a2574
Joe Balistreri <jdbalistreri@gmail.com>
2015-02-05 09:07:10 -0500

    post rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polls::Application.config.secret_key_base = '7c23729b08c3db96074d948a0addb3e7d98d82ec010e205cab1c9a1fc758451ef8c388764b7ca321ba07e9d21f1d4c7e2f69666745bdfe975dd1064e0c02de89'
