b3a65228a25116874e3025856b2a04a2ecf36b9b
Joe Balistreri <jdbalistreri@gmail.com>
2015-02-09 15:42:54 -0500

    Create models and controllers for User, Contact, ContactShares.

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Route66::Application.initialize!
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Route66::Application.routes.draw do
+  resources :users
+  # get 'users'             => 'users#index',   :as => 'users'
+  # post 'users'            => 'users#create'
+  # get 'users/new'         => 'users#new',     :as => 'new_user'
+  # get 'users/:id/edit'    => 'users#edit',    :as => 'edit_user'
+  # get 'users/:id'         => 'users#show',    :as => 'user'
+  # patch 'users/update'    => 'users#update'
+  # put 'users/update'      => 'users#update'
+  # delete 'users/:id'      => 'users#destroy'
+
+end
