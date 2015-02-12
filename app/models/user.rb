226bbc49b60d1e0b675f9c7e85d8e55696c49b67
Joe Balistreri <jdbalistreri@gmail.com>
2015-02-12 11:35:49 -0500

    minor refactoring of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ActiveRecord::Base
 
   def self.find_by_credentials(email, password)
     user = User.find_by_email(email)
-    return nil unless user
-
+    return nil if user.nil?
     user.is_password?(password) ? user : nil
   end
 
@@ -26,7 +25,7 @@ class User < ActiveRecord::Base
   end
 
   def self.generate_session_token
-    SecureRandom.urlsafe_base64
+    SecureRandom::urlsafe_base64(16)
   end
 
   def reset_session_token!
