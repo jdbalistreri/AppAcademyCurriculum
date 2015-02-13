2635f91acd126360503369b3fc4d7bd9529452c3
Joe Balistreri <jdbalistreri@gmail.com>
2015-02-13 11:32:41 -0500

    complete auth protocol

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def auth_token
+    <<-HTML.html_safe
+      <input type="hidden" name="authenticity_token" value="#{form_authenticity_token}">
+    HTML
+  end
+
+  def hidden_delete
+    <<-HTML.html_safe
+      <input type="hidden" name="_method" value="delete">
+    HTML
+  end
+
+  def hidden_patch
+    <<-HTML.html_safe
+      <input type="hidden" name="_method" value="patch"> 
+    HTML
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, :session_token, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6,  allow_null: true }
+  validates :password, length: { minimum: 6,  allow_nil: true }
 
   after_initialize :ensure_session_token
 
@@ -8,6 +8,12 @@ class User < ActiveRecord::Base
     token = SecureRandom::urlsafe_base64
     generate_session_token if User.exists?(session_token: token)
     token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
   end
 
   def reset_session_token!
