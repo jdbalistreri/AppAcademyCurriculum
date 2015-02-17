b52c88acd974a20a3c5752c11551f32becb2291b
Joe Balistreri <jdbalistreri@gmail.com>
2015-02-17 12:30:45 -0500

    finish phase 4

--- lib/phase4/controller_base.rb
+++ lib/phase4/controller_base.rb
@@ -4,13 +4,30 @@ require_relative './session'
 module Phase4
   class ControllerBase < Phase3::ControllerBase
     def redirect_to(url)
+      fail if already_built_response?
+      
+      res.header["location"] = url
+      res.status = 302
+
+      @session.store_session(res)
+
+      @already_built_response = true
     end
 
     def render_content(content, content_type)
+      fail if already_built_response?
+
+      res.content_type = content_type
+      res.body = content
+
+      @session.store_session(res)
+
+      @already_built_response = true
     end
 
     # method exposing a `Session` object
     def session
+      @session ||= Session.new(req)
     end
   end
 end
