2bdd707f0c2c743208c043030603b05b2d8a15fd
Joe Balistreri <jdbalistreri@gmail.com>
2015-02-27 14:30:29 -0500

    finish phase 1 - follow button

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -5,7 +5,6 @@
     this.$el = $(el);
     this.userId = this.$el.data("user-id");
     this.followState = this.$el.data("initial-follow-state");
-    this.request = null;
     this.render();
     this.handleClick();
   };
@@ -13,11 +12,22 @@
 
   $.FollowToggle.prototype.render = function () {
     if (this.followState === "followed") {
+      this.$el.prop("disabled", false);
       this.$el.text("Unfollow!");
       this.request = $.FollowToggle.unfollowRequest;
+
     } else if (this.followState === "unfollowed") {
+      this.$el.prop("disabled", false);
       this.$el.text("Follow!");
       this.request = $.FollowToggle.followRequest;
+
+    } else if (this.followState === "following") {
+      this.$el.text("Following...");
+      this.$el.prop("disabled", true);
+
+    } else if (this.followState === "unfollowing") {
+      this.$el.text("Unfollowing...");
+      this.$el.prop("disabled", true);
     }
   };
 
@@ -32,30 +42,40 @@
 
   $.FollowToggle.followRequest = function () {
     var that = this;
+    this.followState = "following";
+    this.render();
+
     return $.ajax({
       url: "/users/" + this.userId + "/follow",
       type: "post",
       dataType: "json",
-      data: this.$el.serialize(),
       success: function(){
         that.followState = "followed";
         that.render();
         },
-      failure: function(){alert("FAILURE");}
+      failure: function(){
+        that.followState = "unfollowed";
+        that.render();
+      }
     })
   }
 
   $.FollowToggle.unfollowRequest = function () {
     var that = this;
+    this.followState = "unfollowing";
+    this.render();
+
     return $.ajax({
       url: "/users/" + this.userId + "/follow",
       type: "delete",
-      data: this.$el.serialize(),
       dataType: "json",
       success: function () {
         that.followState = "unfollowed";
         that.render();},
-      failure: function(){alert("FAILURE :(");}
+      failure: function(){
+        that.followState = "followed";
+        that.render();
+      }
     })
   }
 
